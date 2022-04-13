2bb56fe8bada5011fae76f7546ab630afe37ac6f
CipiVlad <supercoder.cipi@gmail.com>
2022-04-14 00:05:57 +0200

    js-windowObjectMethod

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+// window.confirm('Are you sure you want to check your settings?')
+// window.prompt('Bitte geben Sie ihren Namen ein', 'hier tippen');
+// window.prompt("Bitte geben Sie Ihren Namen ein", "Cancel");
+// window.alert("Hallo Welt");
+
+// let alter = prompt('Gib mir bitte dein Alter:')
+// console.log(alter);
+
+// let b = 5;
+// let a = b * 5 - 3;
+// alert(a);
+
+let mann = 'Robert Wadlow ist der größte Mann der Welt: ';
+let groesse = "2,72m";
+alert(mann + groesse);
